c86309d23fa6da0078f7ccdb3dbae9c8e513bc18
Abdelrahman <abdelrahman@brown.edu>
2021-02-23 12:46:05 -0500

    structure repo

---
+++ Makefile
@@ -0,0 +1,18 @@
+.PHONY: all test clean
+
+clean:
+	rm -rf dist
+	rm -rf edaac.egg-info
+	rm -rf build
+	
+build:
+	python setup.py sdist bdist_wheel
+
+test:
+	python test.py
+
+check:
+	twine check dist/*
+
+publish: clean build
+	twine upload dist/*
---
+++ edgify/functional/linear.py
@@ -0,0 +1,26 @@
+import torch
+
+class Linear(torch.auto.autograd):
+
+    @staticmethod
+    def forward(ctx, input, weight, bias=None):
+        ctx.save_for_backward(input, weight, bias)
+        output = input.mm(weight.t())
+        if bias is not None:
+            output += bias.unsqueeze(0).expand_as(output)
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input, weight, bias = ctx.saved_tensors
+        grad_input = grad_weight = grad_bias = None
+
+        if ctx.needs_input_grad[0]:
+            grad_input = grad_output.mm(weight)
+        if ctx.needs_input_grad[1]:
+            grad_weight = grad_output.t().mm(input)
+        if bias is not None and ctx.needs_input_grad[2]:
+            grad_bias = grad_output.sum(0)
+        
+        return grad_input, grad_weight, grad_bias
+
