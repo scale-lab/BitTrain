bf301c7cd2124a71089ad80546517d0c3b020386
MarinaHesham <marinahesham21@gmail.com>
2021-02-24 21:53:03 -0500

    add bias only training and freeze feature extractor training

---
+++ baselines/bias_only_training.py
@@ -0,0 +1,47 @@
+import os
+import time
+import copy
+import argparse
+import torch
+from torchvision import datasets, models, transforms
+from utils.datasets import load_data
+from utils.train import train_model
+from edgify.profiling.functions import profile
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument("model", type=str, \
+        help="Name of the model you want from PyTorch model zoo.")
+    args = parser.parse_args()
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    dataloaders, class_names, dataset_sizes = load_data()
+    model = getattr(models, args.model)(pretrained=True)
+
+    # Freeze all the convolutional layers of the network
+    for name, param in model.named_parameters():
+        if "bias" not in name:
+            param.requires_grad = False
+
+    # Add fully connected layers for classification
+    num_ftrs = model.fc.in_features
+    model.fc = torch.nn.Linear(num_ftrs, len(class_names))
+
+    model.to(device)
+
+    # Do memory profiling first
+    inputs, labels = next(iter(dataloaders['val']))
+
+    inputs = inputs.to(device)
+    labels = labels.to(device)
+
+    mem_usage, compute_time = profile(model, inputs, labels, 
+                                      torch.nn.CrossEntropyLoss(), 
+                                      use_cuda=torch.cuda.is_available(),
+                                      export=False)
+    compute_time /= 1000.0
+
+    print(f'Memory usage (Mb): {mem_usage:,}, Compute Time (sec.): {compute_time:.4f} on {device}')
+    
+    # Re-train the last fully connected layers and biases
+    train_model(model, dataloaders, dataset_sizes, device=device, num_epochs=25)
---
+++ baselines/utils/datasets.py
@@ -0,0 +1,32 @@
+import os
+import torch
+from torchvision import datasets, transforms
+
+
+def load_data():
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+
+    data_dir = '../data/hymenoptera_data'
+    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['train', 'val']}
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                             shuffle=True, num_workers=0)
+              for x in ['train', 'val']}
+    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+    class_names = image_datasets['train'].classes
+
+    return dataloaders, class_names, dataset_sizes
