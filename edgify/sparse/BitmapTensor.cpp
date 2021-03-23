8435a8d6701c3864d850092baaba022083af5de1
abdelrahmanhosny <abdelrahman.hosny@hotmail.com>
2021-03-22 23:27:40 -0400

    boilerplate for implementing conv2d in c++

--- edgify/sparse/BitmapTensor.cpp
+++ edgify/sparse/BitmapTensor.cpp
@@ -1,8 +1,10 @@
 #include <vector>
 #include <torch/csrc/autograd/functions/utils.h>
+#include <torch/torch.h>
 #include <boost/dynamic_bitset.hpp>
 #include <torch/extension.h>
 
+using namespace torch::autograd;
 
 class BitmapTensor {
   private:
@@ -14,6 +16,7 @@ class BitmapTensor {
     ~BitmapTensor();
     torch::Tensor get_dense(bool on_gpu);
 };
+
 
 BitmapTensor::BitmapTensor(torch::Tensor t) {
   for (auto s: t.sizes()) {
@@ -42,7 +45,7 @@ BitmapTensor::BitmapTensor(torch::Tensor t) {
       }
     }
   }
-  
+  std::cout << values.size() << "/" << t.numel() << " (" << (float) values.size() / (float) t.numel() * 100.0 << "%)" <<  std::endl;
 }
   
 
@@ -70,9 +73,48 @@ torch::Tensor BitmapTensor::get_dense(bool on_gpu) {
   return t;
 }
 
+
+class LinearFunction : public Function<LinearFunction> {
+ public:
+  // Note that both forward and backward are static functions
+
+  // bias is an optional argument
+  static torch::Tensor forward(
+      AutogradContext *ctx, torch::Tensor input, torch::Tensor weight, torch::Tensor bias = torch::Tensor()) {
+    ctx->save_for_backward({input, weight, bias});
+    auto output = input.mm(weight.t());
+    if (bias.defined()) {
+      output += bias.unsqueeze(0).expand_as(output);
+    }
+    return output;
+  }
+
+  static tensor_list backward(AutogradContext *ctx, tensor_list grad_outputs) {
+    auto saved = ctx->get_saved_variables();
+    auto input = saved[0];
+    auto weight = saved[1];
+    auto bias = saved[2];
+
+    auto grad_output = grad_outputs[0];
+    auto grad_input = grad_output.mm(weight);
+    auto grad_weight = grad_output.t().mm(input);
+    auto grad_bias = torch::Tensor();
+    if (bias.defined()) {
+      grad_bias = grad_output.sum(0);
+    }
+
+    return {grad_input, grad_weight, grad_bias};
+  }
+};
+
+torch::Tensor apply(torch::Tensor input, torch::Tensor weight) {
+  return LinearFunction::apply(input, weight);
+}
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   py::class_<BitmapTensor>(m, "BitmapTensor")
         .def(py::init<const torch::Tensor &>())
         .def("get_dense", &BitmapTensor::get_dense);
+  m.def("apply", &apply, "Apply Linear Function");
 }
 
